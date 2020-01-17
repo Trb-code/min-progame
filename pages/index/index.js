22acecad920d650f52cfc94056fa0b9e358d7462
tangrongbiao <439013864@qq.com>
2020-01-17 15:05:52 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,105 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+  /**
+   * 页面的初始数据
+   */
+  data: {	
+  // 轮播图
+    swiper_list:[],
+    // 导航分类
+    navclassify:[],
+    // 时尚女装
+    fashion:[]
+     
+   
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 轮播图
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (res)=> {
+        // console.log(res),
+          
+        this.setData({   
+          swiper_list:res.data.message,
+          
         })
+        // console.log(this.data.swiper_list)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+// 导航分类
+wx.request({
+  url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+  success:(res)=>{
+    console.log(res)
+    this.setData({
+      navclassify:res.data.message
+    })
+  }
+})
+
+// 时尚女装
+wx.request({
+  url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+  success:(res)=>{
+    console.log(res)
+   this.setData({
+     fashion:res.data.message
+   })
+  }
+}) 
+},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
